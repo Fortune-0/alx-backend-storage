b959c8cb5cdff92012d2447a5f509d57975fb4c7
fortune-0 <fortunepeterspc07@gmail.com>
2024-06-20 03:59:54 +0100

    README.md

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python3
+""" Redis client module
+"""
 import redis
 import uuid
 from typing import Any, Callable, Optional, Union
 
 
 class Cache:
+    """ Caching class
+    """
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Stores data in redis with randomly generated key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-        
         return (key)
