d11ce3c6b20dad07b2cbebd7684f6bde7aa1c0a6
fortune-0 <fortunepeterspc07@gmail.com>
2024-06-20 03:50:16 +0100

    - method that takes a data argument and returns a string

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,15 @@
+import redis
+import uuid
+from typing import Any, Callable, Optional, Union
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb
+    
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        
+        return (key)
